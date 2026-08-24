806a2150296983fc82fe8f21c004e62b8216fae9
yakhlafhoussam <yakhlafhoussam@gmail.com>
2026-08-24 21:21:19 +0100

    Refactor contact handling functions for improved readability and organization

--- index.py
+++ index.py
@@ -1,14 +1,28 @@
 import os
 
+def creatContact():
+    file = open("contact.txt", "w")
+    file.close
+    
+def readContact():
+    file = open("contact.txt", "r")
+    contact = file.read()
+    return contact
+
+def addNewContact(name, phone, email):
+    contact = name + " " + phone + " " + email
+    file = open("contact.txt", "a")
+    file.write(contact)
+
 print("Hello Houssam !")
 
 if os.path.exists("contact.txt"):
     print("The contact file is exist !")
-    file = open("contact.txt", "r")
-    contact = file.read()
+    contact = readContact()
     print(contact)
 else:
     print("The contact file not exist !")
-    file = open("contact.txt", "w")
-    file.close
+    creatContact()
     print("The conatct file was created !")
+    addNewContact("Houssam", "0615940605", "yakhlafhoussam@gmail.com")
+    print("The New contact was created !")
