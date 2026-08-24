09bb111b0a0a793a0f2150fb10b7ac455eacffa6
yakhlafhoussam <yakhlafhoussam@gmail.com>
2026-08-24 20:55:15 +0100

    Add contact file handling and .gitignore for contact.txt

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
+import os
+
 print("Hello Houssam !")
+
+if os.path.exists("contact.txt"):
+    print("The contact file is exist !")
+else:
+    print("The contact file not exist !")
+    file = open("contact.txt", "w")
+    file.close
+    print("The conatct file was created !")
